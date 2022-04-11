2510bd74367d2430409ca7f5c054b8fb229e8e83
Julian Teschner <92teju1bif@hft-stuttgart.de>
2022-04-11 11:10:04 +0200

    add v2 & v3

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -33,19 +33,15 @@ def count_punctuation(
     return data
 
 
-def count_negation(
-    data: pd.DataFrame, negation: List[str]
-) -> pd.DataFrame:
+def count_negation(data: pd.DataFrame, negation: List[str]) -> pd.DataFrame:
     res: list[int] = []
     for entry in data["text"]:
         res.append(len([word for word in entry.split(" ") if word.lower() in negation]))
-    data["Negation"] = res
+    data["negation"] = res
     return data
 
 
-def start_sentence(
-    data: pd.DataFrame, start: List[str], title:str
-) -> pd.DataFrame:
+def start_sentence(data: pd.DataFrame, start: List[str], title: str) -> pd.DataFrame:
     res: list[int] = []
     for entry in data["text"]:
         res.append(len([word for word in entry.split(" ", 1) if word in start]))
@@ -53,22 +49,21 @@ def start_sentence(
     return data
 
 
-def count_uppercase_words(
-    data: pd.DataFrame, exception: List[str]
-) -> pd.DataFrame:
+def count_uppercase_words(data: pd.DataFrame, exception: List[str]) -> pd.DataFrame:
     res: list[int] = []
     for entry in data["text"]:
-        res.append(len([word for word in entry.split(" ") if word.isupper() and word not in exception]))
+        res.append(
+            len(
+                [
+                    word
+                    for word in entry.split(" ")
+                    if word.isupper() and word not in exception
+                ]
+            )
+        )
     data["upper_case"] = res
     return data
 
-def add_quotation_marks(data: pd.DataFrame
-) -> pd.DataFrame:
-    data["text"] = data["text"].astype(str) + "a"
-    data["text"] = "a" + data["text"].astype(str)
-
-    return data
-    
 
 def main():
     data = pd.read_csv("data.csv", quotechar="'")
@@ -82,22 +77,48 @@ def main():
     with open("negative.txt", "r") as f:
         negative_words = f.read().splitlines()
 
-    data = count_words(data)
+    # data = count_words(data)
 
     data = count_sentiment_words(data, positive_words, "positive_words")
     data = count_sentiment_words(data, negative_words, "negative_words")
+    data = count_sentiment_words(
+        data, ["Hardly", "Scarcely", "Barely", "little", "seldom"], "negative_adverbs"
+    )
 
     data = count_punctuation(data, [".", ",", "!", "?"], "punctuation_sum")
-    data = count_punctuation(data, ["."], "punctuation_dot")
-    data = count_punctuation(data, [","], "punctuation_comma")
     data = count_punctuation(data, ["!"], "punctuation_exclamation")
-    data = count_punctuation(data, ["?"], "punctuation_question")
-    
-    data = count_negation(data, ["not", "no"])
-    
-    data = start_sentence(data, ["I"], "start_with_I")
-    
-    data = count_uppercase_words(data, ["I", "A"])
+    # data = count_punctuation(data, ["."], "punctuation_dot")
+    # data = count_punctuation(data, [","], "punctuation_comma")
+    # data = count_punctuation(data, ["?"], "punctuation_question")
+
+    data = count_negation(
+        data,
+        [
+            "not",
+            "no",
+            "n't",
+            'n"t',
+            "never",
+            "neither",
+            "nor",
+            "None",
+            "nothing",
+            "No one.",
+            "nowhere",
+        ],
+    )
+
+    # data = start_sentence(data, ["I"], "start_with_I")
+
+    # data = count_uppercase_words(data, ["I", "A"])
+
+    cols = data.columns.tolist()
+
+    pred = data["class"]
+    data = data.drop(["class"], axis=1)
+    data["class"] = pred
+
+    print(data.columns.tolist())
 
     print(data)
     # Write to file
@@ -108,8 +129,9 @@ def main():
 
     print(data["text"])
 
-    with open("mod_data_first_iteration.arff", mode="w", encoding="utf-8") as dst:
+    with open("mod_data_first_iteration_v3.arff", mode="w", encoding="utf-8") as dst:
         data.to_csv(dst, quoting=csv.QUOTE_NONNUMERIC, quotechar="'", index=False)
+
 
 if __name__ == "__main__":
     main()
